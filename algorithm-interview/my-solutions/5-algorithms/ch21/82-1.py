e70e8397951d884c367fb03bc5714e2fd4c72dce
yg-moon <yg-moon@snu.ac.kr>
2022-05-13 12:56:28 +0900

    Fix comment

--- algorithm-interview/my-solutions/5-algorithms/ch21/82-1.py
+++ algorithm-interview/my-solutions/5-algorithms/ch21/82-1.py
@@ -7,7 +7,7 @@ class Solution:
         s.sort()
 
         child_i = cookie_j = 0
-        # 쿠키는 계속 올리고, 아이는 만족했을 때만 올리기
+        # 투 포인터로 쿠키는 계속 올리고, 아이는 만족했을 때만 올리기
         while child_i < len(g) and cookie_j < len(s):
             if s[cookie_j] >= g[child_i]:
                 child_i += 1
