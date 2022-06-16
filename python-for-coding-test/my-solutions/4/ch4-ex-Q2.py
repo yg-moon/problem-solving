577c91c83297a8c36404e8334561b9472bb49f80
yg-moon <yg-moon@snu.ac.kr>
2022-06-17 00:22:53 +0900

    Refactor 'while' into 'for'

--- python-for-coding-test/my-solutions/4/ch4-ex-Q2.py
+++ python-for-coding-test/my-solutions/4/ch4-ex-Q2.py
@@ -1,20 +1,10 @@
 # 같은 풀이지만, 책 구현이 더 깔끔.
 n = int(input())
-
-hour = 0
-minute = 0
-second = 0
 cnt = 0
 
-while hour <= n:
-    hour += 1
-    minute = 0
-    while minute <= 59:
-        minute += 1
-        second = 0
-        while second <= 59:
+for hour in range(n + 1):
+    for minute in range(60):
+        for second in range(60):
             if "3" in str(hour) + str(minute) + str(second):
                 cnt += 1
-            second += 1
-
 print(cnt)
