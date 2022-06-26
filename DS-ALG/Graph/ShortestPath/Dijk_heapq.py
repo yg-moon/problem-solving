59e7e98b65a1f2ef52b9c85c10d736acce9a803e
yg-moon <yg-moon@snu.ac.kr>
2022-06-26 15:20:33 +0900

    Update dijkstra codes

--- DS-ALG/Graph/ShortestPath/Dijk_heapq.py
+++ DS-ALG/Graph/ShortestPath/Dijk_heapq.py
@@ -1,52 +1,43 @@
-# https://freedeveloper.tistory.com/384
+# 이코테 버전
+# - dist를 무한으로 채우고 시작한다.
+# - 거리를 비교해서 짧을 때만 relaxation 한다.
 
+import collections
 import heapq
-import sys
-input = sys.stdin.readline
-INF = int(1e9) # 무한을 의미하는 값으로 10억을 설정
+from typing import List
+INF = int(1e9)
 
-# 노드의 개수, 간선의 개수를 입력받기
-n, m = map(int, input().split())
-# 시작 노드 번호를 입력받기
-start = int(input())
-# 각 노드에 연결되어 있는 노드에 대한 정보를 담는 리스트를 만들기
-graph = [[] for i in range(n + 1)]
-# 최단 거리 테이블을 모두 무한으로 초기화
-distance = [INF] * (n + 1)
 
-# 모든 간선 정보를 입력받기
-for _ in range(m):
-    a, b, c = map(int, input().split())
-    # a번 노드에서 b번 노드로 가는 비용이 c라는 의미
-    graph[a].append((b, c))
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], N: int, K: int) -> int:
+        # 그래프 구성
+        graph = collections.defaultdict(list)
+        for u, v, w in times:
+            graph[u].append((v, w))
 
-def dijkstra(start):
-    q = []
-    # 시작 노드로 가기 위한 최단 경로는 0으로 설정하여, 큐에 삽입
-    heapq.heappush(q, (0, start))
-    distance[start] = 0
-    while q: # 큐가 비어있지 않다면
-        # 가장 최단 거리가 짧은 노드에 대한 정보 꺼내기
-        dist, now = heapq.heappop(q)
-        # 현재 노드가 이미 처리된 적이 있는 노드라면 무시
-        if distance[now] < dist:
-            continue
-        # 현재 노드와 연결된 다른 인접한 노드들을 확인
-        for i in graph[now]:
-            cost = dist + i[1]
-            # 현재 노드를 거쳐서, 다른 노드로 이동하는 거리가 더 짧은 경우
-            if cost < distance[i[0]]:
-                distance[i[0]] = cost
-                heapq.heappush(q, (cost, i[0]))
+        # 최단거리 정보
+        dist = collections.defaultdict(int)
+        for i in range(1, N + 1):
+            dist[i] = INF
+        dist[K] = 0
 
-# 다익스트라 알고리즘을 수행
-dijkstra(start)
+        # 큐: [(소요 시간, 노드)]
+        Q = [(0, K)]
 
-# 모든 노드로 가기 위한 최단 거리를 출력
-for i in range(1, n + 1):
-    # 도달할 수 없는 경우, 무한(INFINITY)이라고 출력
-    if distance[i] == INF:
-        print("INFINITY")
-    # 도달할 수 있는 경우 거리를 출력
-    else:
-        print(distance[i])
+        # 우선순위 큐를 이용, 가장 가까운 노드 찾기
+        while Q:
+            time, node = heapq.heappop(Q)
+            # 현재 노드가 이미 처리된 적이 있으면 무시
+            if dist[node] < time:
+                continue
+            # relaxation
+            for v, w in graph[node]:
+                alt = time + w
+                if alt < dist[v]:
+                    dist[v] = alt
+                    heapq.heappush(Q, (alt, v))
+
+        # 도달할 수 없는 노드 존재여부 확인
+        if INF in dist.values():
+            return -1
+        return max(dist.values())
