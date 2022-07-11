3ce436e5efd811860a9e25d43fa9b341d46375e7
yg-moon <yg-moon@snu.ac.kr>
2022-07-11 19:29:54 +0900

    Rename variables

--- python-for-coding-test/my-solutions/12/Q10.py
+++ python-for-coding-test/my-solutions/12/Q10.py
@@ -16,30 +16,30 @@ def solution(key, lock):
     lock_zero_pos = []  # 자물쇠에서 0인 좌표 목록
 
     # 자물쇠를 padding: 범위 밖은 2로 처리
-    for i in range(P):
-        for j in range(P):
-            if i < M - 1 or i > P - M or j < M - 1 or j > P - M:
-                graph[i][j] = 2
+    for x in range(P):
+        for y in range(P):
+            if x < M - 1 or x > P - M or y < M - 1 or y > P - M:
+                graph[x][y] = 2
             else:
-                graph[i][j] = lock[i - (M - 1)][j - (M - 1)]
-                if graph[i][j] == 0:
-                    lock_zero_pos.append((i, j))
+                graph[x][y] = lock[x - (M - 1)][y - (M - 1)]
+                if graph[x][y] == 0:
+                    lock_zero_pos.append((x, y))
     lock_zero_pos.sort()
 
     # 돌려가면서 열쇠 꽂기
     answer = False
     for _ in range(4):
         key = rotate(key)
-        for i in range(P - (M - 1)):  # 주의: range(P)로 하면 범위를 벗어남
-            for j in range(P - (M - 1)):
+        for x in range(P - (M - 1)):  # 주의: range(P)로 하면 범위를 벗어남
+            for y in range(P - (M - 1)):
                 key_match_pos = []  # 짝이 맞는 좌표 목록
                 overlap = False  # 돌기가 겹치는지 확인
-                for x in range(M):
-                    for y in range(M):
-                        # 주의: mat[i][j]가 아님!
-                        if graph[i + x][j + y] == 0 and key[x][y] == 1:
-                            key_match_pos.append((i + x, j + y))
-                        elif graph[i + x][j + y] == 1 and key[x][y] == 1:
+                for a in range(M):
+                    for b in range(M):
+                        # 주의: mat[x][y]가 아님!
+                        if graph[x + a][y + b] == 0 and key[a][b] == 1:
+                            key_match_pos.append((x + a, y + b))
+                        elif graph[x + a][y + b] == 1 and key[a][b] == 1:
                             overlap = True
                 key_match_pos.sort()
                 if not overlap and lock_zero_pos == key_match_pos:
