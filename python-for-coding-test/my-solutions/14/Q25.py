958fcff49c87ac5702f2813d5937bd26899b7909
yg-moon <yg-moon@snu.ac.kr>
2022-07-15 03:14:02 +0900

    Update comment

--- python-for-coding-test/my-solutions/14/Q25.py
+++ python-for-coding-test/my-solutions/14/Q25.py
@@ -4,12 +4,14 @@ from collections import defaultdict
 def solution(N, stages):
     # {스테이지: 도달 인원수}
     dic = defaultdict(int)
+    # stage 배열에 대해 돌면서 ‘도달 인원수’를 계산
     for i in range(len(stages)):
         dic[stages[i]] += 1
 
     # [(스테이지, 실패율)]
     answer = []
     total = len(stages)
+    # 각 스테이지 번호에 대해 돌면서 ‘실패율’을 계산
     for i in range(1, N + 1):
         if i in dic:
             fail = dic[i] / total
