11bc2bd4d3e9713600b8af980f95a4bcc741f2ab
yg-moon <yg-moon@snu.ac.kr>
2022-07-20 12:58:27 +0900

    Remove empty line

--- programmers/kakao/2022-blind/Q2.py
+++ programmers/kakao/2022-blind/Q2.py
@@ -12,7 +12,6 @@ def solution(n, k):
     def isPrime(n):
         if n < 2:
             return False
-
         for i in range(2, int(n**0.5) + 1):
             if n % i == 0:
                 return False
