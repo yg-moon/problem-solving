e16ddcbe37ecd0f2fd0156b4750ff4c5bb4e9b09
yg-moon <yg-moon@snu.ac.kr>
2022-07-28 01:33:32 +0900

    Add Floyd-Warshall algorithm to snippet

---
+++ snippets/floyd-warshall.py
@@ -0,0 +1,33 @@
+INF = int(1e9)
+
+N = int(input())
+M = int(input())
+
+# INF로 초기화
+graph = [[INF] * (N + 1) for _ in range(N + 1)]
+
+# 자기 자신은 0
+for i in range(1, N + 1):
+    for j in range(1, N + 1):
+        if i == j:
+            graph[i][j] = 0
+
+# 간선 정보 입력
+for _ in range(M):
+    a, b, c = map(int, input().split())
+    graph[a][b] = c
+
+# 플로이드 워셜
+for k in range(1, N + 1):
+    for i in range(1, N + 1):
+        for j in range(1, N + 1):
+            graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
+
+# 출력
+for i in range(1, N + 1):
+    for j in range(1, N + 1):
+        if graph[i][j] == INF:
+            print("%-5s" % "INF", end=" ")
+        else:
+            print("%-5d" % graph[i][j], end=" ")
+    print()
