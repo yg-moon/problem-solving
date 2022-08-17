03db0fcf23c0c33b619247a65e6e0cf74facc4b3
yg-moon <yg-moon@snu.ac.kr>
2022-08-17 21:29:30 +0900

    Update ch5 solutions

--- python-for-coding-test/my-solutions/5/ch5-Q4.py
+++ python-for-coding-test/my-solutions/5/ch5-Q4.py
@@ -1,28 +1,31 @@
-import collections
+from collections import deque
 
-n, m = map(int, input().split())
-graph = [list(map(int, input())) for _ in range(n)]
+N, M = map(int, input().split())
+graph = [list(map(int, input())) for _ in range(N)]
 
 # 시계방향
 dx = [-1, 0, 1, 0]
 dy = [0, 1, 0, -1]
 
-def bfs(i, j):
-    # 현재 좌표와 count를 함께 큐에 저장
-    q = collections.deque([(i, j, 1)])
+
+def bfs(x, y):
+    # 큐: 현재 좌표와 count를 함께 저장
+    Q = deque([(x, y, 1)])
     # 방문한 곳은 2로 표시
-    graph[i][j] = 2
-    while q:
-        qi, qj, qcnt = q.popleft()
+    graph[x][y] = 2
+    while Q:
+        qx, qy, qcnt = Q.popleft()
         for k in range(4):
-            ni = qi + dx[k]
-            nj = qj + dy[k]
-            if 0 <= ni < n and 0 <= nj < m and graph[ni][nj] == 1:
-                if ni == n - 1 and nj == m - 1:
+            nx = qx + dx[k]
+            ny = qy + dy[k]
+            if 0 <= nx < N and 0 <= ny < M and graph[nx][ny] == 1:
+                # 최적화: 답을 찾으면 즉시 종료
+                if nx == N - 1 and ny == M - 1:
                     return qcnt + 1
-                graph[qi][qj] = 2
-                q.append((ni, nj, qcnt + 1))
+                graph[qx][qy] = 2
+                Q.append((nx, ny, qcnt + 1))
     # 미로의 정답이 없는 경우
     return -1
+
 
 print(bfs(0, 0))
