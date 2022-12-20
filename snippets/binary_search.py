889413ffbceb121216e112a6b653b977ec8a4d3c
yg-moon <yg-moon@snu.ac.kr>
2022-12-20 18:28:32 +0900

    Add binary search snippets

---
+++ snippets/binary_search.py
@@ -0,0 +1,11 @@
+def search(arr, target):
+    left, right = 0, len(arr) - 1
+    while left <= right:
+        mid = left + (right - left) // 2
+        if arr[mid] < target:
+            left = mid + 1
+        elif arr[mid] > target:
+            right = mid - 1
+        else:
+            return mid
+    return -1
---
+++ snippets/count_by_range.py
@@ -0,0 +1,7 @@
+from bisect import bisect_left, bisect_right
+
+# [left, right] 범위에 있는 데이터의 개수를 리턴
+def count_by_range(arr, left, right):
+    left_idx = bisect_left(arr, left)
+    right_idx = bisect_right(arr, right)
+    return right_idx - left_idx
