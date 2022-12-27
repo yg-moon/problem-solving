e4cadb899eb56cdbbe4025fadb8c9df22aa97226
yg-moon <yg-moon@snu.ac.kr>
2022-12-27 20:13:25 +0900

    Update sorting codes

--- DS-ALG/Sorting/InsertionSort.py
+++ DS-ALG/Sorting/InsertionSort.py
@@ -3,30 +3,31 @@
 # Function to do insertion sort
 def insertionSort(arr):
 
-	# Traverse through 1 to len(arr)
-	for i in range(1, len(arr)):
+    # Traverse through 1 to len(arr)
+    for i in range(1, len(arr)):
 
-		key = arr[i]
+        key = arr[i]
 
-		# Move elements of arr[0..i-1], that are
-		# greater than key, to one position ahead
-		# of their current position
-		j = i-1
-		while j >= 0 and key < arr[j] :
-				arr[j + 1] = arr[j]
-				j -= 1
-		arr[j + 1] = key
+        # Move elements of arr[0..i-1], that are
+        # greater than key, to one position ahead
+        # of their current position
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
 
 
 # Driver code to test above
 arr = [12, 11, 13, 5, 6]
 insertionSort(arr)
 for i in range(len(arr)):
-	print ("% d" % arr[i])
+    print("% d" % arr[i])
 
 # This code is contributed by Mohit Kumra
 
-# - 세부 구현:
+# - 요약: 한칸씩 오른쪽으로 올려주고, 마지막 자리에 key를 넣는 방식.
+# - 구현:
 #   - (i = 1) 부터 시작, (j = i-1) 이다.
 #   - loop 동안은 arr[j+1]의 값만 업데이트하고, j -= 1.
 #   - loop 종료 후 arr[j+1] = key 로 'insert' 해준다.
