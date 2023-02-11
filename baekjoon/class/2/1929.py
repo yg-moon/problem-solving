fb3758a7f6bc07a8afc4cbf6147c0b3693b1fd12
yg-moon <yg-moon@snu.ac.kr>
2023-02-11 15:07:53 +0900

    Update prime related codes

--- baekjoon/class/2/1929.py
+++ baekjoon/class/2/1929.py
@@ -1,13 +1,13 @@
 # 소수 구하기
 M, N = map(int, input().split())
-arr = [True for _ in range(N + 1)]
+arr = [True] * (N + 1)
 arr[0] = False
 arr[1] = False
 
 # 에라토스테네스의 체
 for i in range(2, int(N**0.5) + 1):
     if arr[i]:
-        # 핵심: i*2부터 N까지, i씩 증가하며 검사
+        # 핵심: i*2부터 N까지, i씩 증가하며 처리
         for j in range(i * 2, N + 1, i):
             arr[j] = False
 
---
+++ snippets/prime.py
@@ -0,0 +1,24 @@
+# 효과적인 소수 판별 알고리즘: 제곱근까지만 판별하면 된다.
+# 이유: 모든 약수는 가운데 수를 기준으로 ‘곱셈 연산에 대해 대칭’을 이루는 성질이 있기 때문.
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+
+# 에라토스테네스의 체
+def prime_list(n):
+    arr = [True] * (n + 1)
+    arr[0] = False
+    arr[1] = False
+
+    for i in range(2, int(n**0.5) + 1):
+        if arr[i] == True:
+            # 핵심: i*2부터 n까지, i씩 증가하며 처리
+            for j in range(i * 2, n + 1, i):
+                arr[j] = False
+
+    return [i for i in range(n + 1) if arr[i]]
