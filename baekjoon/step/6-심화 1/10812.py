03dd3e1340a103aa510366c654842d2f10934c18
yg-moon <yg-moon@snu.ac.kr>
2023-02-21 13:04:24 +0900

    Update boj step 1~9 solutions

---
+++ baekjoon/step/6-심화 1/10812.py
@@ -0,0 +1,19 @@
+# 바구니 순서 바꾸기
+N, M = map(int, input().split())
+arr = [0] * N
+
+for i in range(N):
+    arr[i] = i + 1
+
+for _ in range(M):
+    i, j, k = map(int, input().split())
+    arr[i - 1 : j] = arr[k - 1 : j] + arr[i - 1 : k - 1]
+
+print(*arr)
+
+"""
+- 난이도: 브론즈2
+- 분류: 구현
+
+- 주의: 파이썬 슬라이싱 범위 생각하기. (끝범위 미포함)
+"""
