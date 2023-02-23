3589e0c4abd8f910fd487afe1ae9d53a4027da20
yg-moon <yg-moon@snu.ac.kr>
2023-02-23 14:26:55 +0900

    Update boj step 1~9 solutions

---
+++ baekjoon/step/7-2차원 배열/10798.py
@@ -0,0 +1,22 @@
+# 세로읽기
+arr = [[""] * 15 for _ in range(5)]
+
+for i in range(5):
+    s = input()
+    for j, c in enumerate(s):
+        arr[i][j] = c
+
+answer = ""
+
+for j in range(15):
+    for i in range(5):
+        answer += arr[i][j]
+
+print(answer)
+
+"""
+- 난이도: 브론즈1
+- 분류: 구현
+
+- 5x15 2차원 배열을 선언해서 푼다.
+"""
