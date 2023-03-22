54eb61bf9836cd7f7f6361dbf9319ae4bd70f141
yg-moon <yg-moon@snu.ac.kr>
2023-03-22 17:16:15 +0900

    Add gcd solutions

---
+++ baekjoon/step/lv2/정수론-조합론/2485.py
@@ -0,0 +1,28 @@
+# 가로수
+import math
+
+N = int(input())
+arr = []
+for _ in range(N):
+    arr.append(int(input()))
+
+# 정렬
+arr.sort()
+
+# 인접한 가로수끼리의 차를 구함
+diffs = []
+for i in range(1, N):
+    diffs.append(arr[i] - arr[i - 1])
+
+# 차의 gcd를 구함
+gcd = diffs[0]
+for d in diffs:
+    gcd = math.gcd(gcd, d)
+
+# 전체간격을 gcd로 나누어 더 심을 가로수 파악
+print((arr[-1] - arr[0]) // gcd + 1 - N)
+
+"""
+- 난이도: 실버4
+- 분류: 정수론
+"""
--- snippets/gcd-lcm.py
+++ snippets/gcd-lcm.py
@@ -1,23 +1,30 @@
-def gcd(x, y):
-    # Euclidean algorithm
-    while y:
-        x, y = y, x % y
-    return x
+def gcd(a, b):
+    # Euclidean algorithm (iterative)
+    while b != 0:
+        a, b = b, a % b
+    return a
 
 
-def lcm(x, y):
-    lcm = (x * y) // gcd(x, y)
+def gcd_2(a, b):
+    # Euclidean algorithm (recursive)
+    if a % b == 0:
+        return b
+    return gcd_2(b, a % b)
+
+
+def lcm(a, b):
+    lcm = (a * b) // gcd(a, b)
     return lcm
 
 
-def manual_lcm(x, y):
-    if x > y:
-        greater = x
+def manual_lcm(a, b):
+    if a > b:
+        greater = a
     else:
-        greater = y
+        greater = b
 
     while True:
-        if (greater % x == 0) and (greater % y == 0):
+        if (greater % a == 0) and (greater % b == 0):
             lcm = greater
             break
         greater += 1
