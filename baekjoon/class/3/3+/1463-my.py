df598ffc22a972c2c9e5e9e23b5d784d07402912
yg-moon <yg-moon@snu.ac.kr>
2023-04-05 10:38:55 +0900

    Fix for clause

--- baekjoon/class/3/3+/1463-my.py
+++ baekjoon/class/3/3+/1463-my.py
@@ -8,7 +8,7 @@ dp = [INF] * (N + 1)
 dp[N] = 0
 
 # N부터 1까지 내려가면서 dp를 갱신
-for i in reversed(range(1, N + 1)):
+for i in range(N, 0, -1):
     dp[i - 1] = min(dp[i - 1], dp[i] + 1)
     if i % 2 == 0:
         dp[i // 2] = min(dp[i // 2], dp[i] + 1)
