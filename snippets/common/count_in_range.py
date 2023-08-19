b828120578275ab030a5bf1fe96a85f613287ecf
yg-moon <yg-moon@snu.ac.kr>
2023-08-19 10:29:10 +0900

    Change function name

--- snippets/common/count_in_range.py
+++ snippets/common/count_in_range.py
@@ -1,7 +1,8 @@
 from bisect import bisect_left, bisect_right
 
+
 # [left, right] 범위에 있는 데이터의 개수를 리턴
-def count_by_range(arr, left, right):
+def count_in_range(arr, left, right):
     left_idx = bisect_left(arr, left)
     right_idx = bisect_right(arr, right)
     return right_idx - left_idx
