89a31d49575bbe13838b6338db8de203f81c94ea
yg-moon <yg-moon@snu.ac.kr>
2023-08-20 11:05:30 +0900

    Change variable name to make clear

--- snippets/graph/dijkstra.py
+++ snippets/graph/dijkstra.py
@@ -13,23 +13,23 @@ for _ in range(E):
     graph[u].append((v, w))
 
 
-def dijk(start):
+def dijkstra(start):
     dist = [INF] * (V + 1)
     dist[start] = 0
     pq = [(0, start)]
     while pq:
-        time, node = heapq.heappop(pq)
-        if dist[node] < time:
+        cur_wei, cur_node = heapq.heappop(pq)
+        if dist[cur_node] < cur_wei:
             continue
-        for v, w in graph[node]:
-            alt = time + w
-            if dist[v] > alt:
-                dist[v] = alt
-                heapq.heappush(pq, (alt, v))
+        for nxt_node, nxt_wei in graph[cur_node]:
+            new_wei = cur_wei + nxt_wei
+            if dist[nxt_node] > new_wei:
+                dist[nxt_node] = new_wei
+                heapq.heappush(pq, (new_wei, nxt_node))
     return dist
 
 
-result = dijk(K)
+result = dijkstra(K)
 
 for i in range(1, V + 1):
-    print("INF" if result[i] == INF else result[i])
+    print(result[i] if result[i] != INF else "INF")
