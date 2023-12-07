4ab3b6d1dd892d25d1a23ae9b5624679edb43c4b
yg-moon <yg-moon@snu.ac.kr>
2023-12-07 18:34:49 +0900

    Update boj dp solutions

--- baekjoon/step/lv2+/dp1/9184.py
+++ baekjoon/step/lv2+/dp1/9184.py
@@ -1,7 +1,6 @@
 # 신나는 함수 실행
-# 출처: https://jainn.tistory.com/82
 
-# 핵심1: dp 범위는 0~20만 고려하면 됨
+# 핵심1: 범위는 0~20만 고려하면 됨
 # (0보다 작으면 1을 리턴하고, 20보다 크면 w(20,20,20)으로 통일하기 때문)
 dp = [[[0] * 21 for _ in range(21)] for _ in range(21)]
 
@@ -11,20 +10,21 @@ def w(a, b, c):
         return 1
     if a > 20 or b > 20 or c > 20:
         return w(20, 20, 20)
-    # 핵심2: 이미 계산한 적이 있으면 그대로 리턴
+    # 핵심2: 이미 계산한 적이 있으면, 그대로 리턴
     if dp[a][b][c]:
         return dp[a][b][c]
-    # 핵심3: dp 배열에 계산한 값을 기록해두고 리턴
+    # 핵심3: 아직 계산한 적이 없으면, dp 배열에 값을 기록하고 리턴
     if a < b < c:
         dp[a][b][c] = w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c)
         return dp[a][b][c]
-    dp[a][b][c] = (
-        w(a - 1, b, c)
-        + w(a - 1, b - 1, c)
-        + w(a - 1, b, c - 1)
-        - w(a - 1, b - 1, c - 1)
-    )
-    return dp[a][b][c]
+    else:
+        dp[a][b][c] = (
+            w(a - 1, b, c)
+            + w(a - 1, b - 1, c)
+            + w(a - 1, b, c - 1)
+            - w(a - 1, b - 1, c - 1)
+        )
+        return dp[a][b][c]
 
 
 while True:
@@ -37,5 +37,6 @@ while True:
 - 난이도: 실버2
 - 분류: dp
 
-- Top-Down DP의 좋은 예시
+- Top-Down DP의 기본을 배우는 예제
+- 출처: https://jainn.tistory.com/82
 """
---
+++ baekjoon/tags/dp/14501.py
@@ -0,0 +1,31 @@
+# 퇴사
+N = int(input())
+T = []
+P = []
+for _ in range(N):
+    t, p = map(int, input().split())
+    T.append(t)
+    P.append(p)
+
+# dp[i]: i번째 날에 얻을 수 있는 최대 수익
+dp = [0] * (N + 1)
+max_val = 0
+
+for i in range(N + 1):
+    # 현재 날짜는 지금까지의 최댓값으로 채우기
+    max_val = max(max_val, dp[i])
+    dp[i] = max_val
+    # 상담을 완료하는 날이 퇴사일을 넘기지 않는다면
+    if i < N and i + T[i] <= N:
+        # 해당 날짜에 대해, (그냥 놔두기 vs 상담하기) 중에서 수익이 큰쪽을 선택
+        dp[i + T[i]] = max(dp[i + T[i]], dp[i] + P[i])
+
+print(dp[N])
+
+"""
+- 난이도: 실버3
+- 분류: dp
+
+- 요약: 앞에서부터 채우는 바텀업 dp
+- 참고: https://codejin.tistory.com/173
+"""
