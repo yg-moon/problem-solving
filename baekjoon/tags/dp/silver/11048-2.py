8f0f5a86a58d5e6d0ac3179696e7618c0d8e85d4
yg-moon <yg-moon@snu.ac.kr>
2024-02-06 16:50:52 +0900

    Update 11048

--- baekjoon/tags/dp/silver/11048-2.py
+++ baekjoon/tags/dp/silver/11048-2.py
@@ -1,33 +1,33 @@
-# 이동하기
 import sys
 
-sys.setrecursionlimit(10**6)
+sys.setrecursionlimit(10**5)
 
 N, M = map(int, input().split())
 graph = [list(map(int, input().split())) for _ in range(N)]
 
-# dp[i][j]: (i,j)까지 이동했을 때 가능한 최대 사탕개수
+# dp[i][j]: (i,j)에서 출발했을때 가능한 최대 사탕개수
 dp = [[-1] * M for _ in range(N)]
-# 초기화
-dp[0][0] = graph[0][0]
 
 
-def solve(x, y):
-    if not (0 <= x < N and 0 <= y < M):
+def dfs(x, y):
+    # base case
+    if x >= N or y >= M:
         return 0
 
+    # top-down
     if dp[x][y] != -1:
         return dp[x][y]
 
-    dp[x][y] = graph[x][y] + max(solve(x, y - 1), solve(x - 1, y), solve(x - 1, y - 1))
+    # 점화식
+    dp[x][y] = max(dp[x][y], graph[x][y] + dfs(x + 1, y))
+    dp[x][y] = max(dp[x][y], graph[x][y] + dfs(x, y + 1))
+    dp[x][y] = max(dp[x][y], graph[x][y] + dfs(x + 1, y + 1))
 
     return dp[x][y]
 
 
-print(solve(N - 1, M - 1))
+print(dfs(0, 0))
 
 """
 - 탑다운 풀이
-    - 입력이 크면 재귀스택이 터지므로 주의
-    - 지금처럼 점화식이 같은 경우에는 바텀업이 나음
 """
