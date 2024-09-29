a71ed4e6c04ca0726adca181d9e10366b035d8b7
yg-moon <yg-moon@snu.ac.kr>
2024-09-29 16:28:33 +0900

    Update N-Queens

--- 2-websites/baekjoon/class/4/4+/9663-1.py
+++ 2-websites/baekjoon/class/4/4+/9663-1.py
@@ -1,11 +1,11 @@
-# Naive version
+# 나이브 버전: 시간초과
 def is_safe(r, c):
-    # Check the current row on the left side
+    # 현재 행의 좌측 확인
     for j in range(c):
         if board[r][j] == 1:
             return False
 
-    # Check upper diagonal on the left side
+    # 좌측 상단 대각선 확인
     i = r
     j = c
     while i >= 0 and j >= 0:
@@ -14,7 +14,7 @@ def is_safe(r, c):
         i -= 1
         j -= 1
 
-    # Check lower diagonal on the left side
+    # 좌측 하단 대각선 확인
     i = r
     j = c
     while i < N and j >= 0:
@@ -27,9 +27,9 @@ def is_safe(r, c):
 
 
 def solve(col):
-    global cnt
+    global answer
     if col == N:
-        cnt += 1
+        answer += 1
         return
     for row in range(N):
         if is_safe(row, col):
@@ -40,12 +40,13 @@ def solve(col):
 
 N = int(input())
 board = [[0 for _ in range(N)] for _ in range(N)]
-cnt = 0
+answer = 0
 
 solve(0)
-print(cnt)
+print(answer)
 
 """
-- 첫 column 부터 세로로 한줄씩 채워나가며 확인하는 방식 (주의: row 부터 채워나가는게 아님)
-- 다만 모든 row, col을 확인하기 때문에 비효율적이라서 백준에서는 시간초과.
+- 세로로 한줄씩 채워나가며 확인하는 방식
+- 퀸을 채우는 순서 때문에, 왼쪽 방향만 확인하면 됨
+- 다만 모든 row, col을 직접 확인하기 때문에 비효율적
 """
--- 2-websites/baekjoon/class/4/4+/9663-3.py
+++ 2-websites/baekjoon/class/4/4+/9663-3.py
@@ -1,8 +1,8 @@
-# Optimized version
+# 최적화 버전
 def solve(col):
-    global cnt
+    global answer
     if col == N:
-        cnt += 1
+        answer += 1
         return
     for row in range(N):
         # is_safe를 대신함
@@ -17,25 +17,25 @@ def solve(col):
 
 N = int(input())
 board = [[0 for _ in range(N)] for _ in range(N)]
-cnt = 0
+answer = 0
 
 columns = [False] * N
 diag1 = [False] * (2 * N - 1)  # 주대각선
 diag2 = [False] * (2 * N - 1)  # 역대각선
 
 solve(0)
-print(cnt)
+print(answer)
 
 """
-- 첫 column 부터 세로로 한줄씩 채워나가며 확인하는 방식 (row, col 위치만 바꾸면 반대로도 간단히 가능)
+- 첫 col부터 세로로 한줄씩 채워나가며 확인
 - 현재 상태(열, 대각, 역대각)를 저장하여 중복 확인을 줄여서 효율성 개선
 
 배운점
-- diag1[row - col + N - 1]로 하나의 주대각선(왼쪽 위~ 오른쪽 아래)에 대해 유일한 좌표를 얻을 수 있다.
-    - 주대각선은 모든 i와 j의 차이가 같기 때문
+- diag1[row - col + N - 1]로 하나의 주대각선(왼쪽 위~ 오른쪽 아래)을 전부 커버할 수 있다.
+    - 주대각선은 모든 i-j값이 동일하기 때문
     - N - 1을 더하는 이유는 음수가 되지 않게 하기 위해서
-- diag2[row + col]로 하나의 역대각선에 대해 유일한 좌표를 얻을 수 있다.
-    - 역대각선은 모든 i와 j의 합이 같기 때문
+- diag2[row + col]로 하나의 역대각선을 전부 커버할 수 있다.
+    - 역대각선은 모든 i+j의 값이 동일하기 때문
 - 대각선의 총 개수는 2N-1개 (첫 행과 첫 열에서 시작하는 대각선의 개수를 세보면 됨)
 
 예시
