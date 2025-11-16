3e5eb7e2f034d6bd7e568587642d2b8c052de5f5
yg-moon <yg-moon@snu.ac.kr>
2025-11-17 00:48:54 +0900

    CTCI problems

--- 2-websites/leetcode/ctci/ch1-Arrays and Strings/1.6 String Compression - 443. String Compression.py
+++ 2-websites/leetcode/ctci/ch1-Arrays and Strings/1.6 String Compression - 443. String Compression.py
@@ -45,29 +45,29 @@ class Solution:
     # Sol. 투포인터
     # O(1) space
     def compress(self, chars: List[str]) -> int:
-        i = 0  # 문자열을 훑을 포인터1
-        answer = 0  # 정답을 작성할 포인터2
+        read = 0  # 문자열을 훑을 포인터1
+        write = 0  # 정답을 작성할 포인터2
 
-        while i < len(chars):
-            cur = chars[i]
+        while read < len(chars):
+            char = chars[read]
             cnt = 0
 
             # 카운트 올리기
-            while i < len(chars) and chars[i] == cur:
+            while read < len(chars) and chars[read] == char:
                 cnt += 1
-                i += 1
+                read += 1
 
             # 문자 작성
-            chars[answer] = cur
-            answer += 1
+            chars[write] = char
+            write += 1
 
             # 숫자 작성
             if cnt > 1:
-                for c in str(cnt):
-                    chars[answer] = c
-                    answer += 1
+                for digit in str(cnt):
+                    chars[write] = digit
+                    write += 1
 
-        return answer
+        return write
 
 
 """
